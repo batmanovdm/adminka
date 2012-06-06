dbfb74ee9f3e9aa3f1034bdfba69210615efd60f
batmanovdm <batmanovdm@gmail.com>
2012-06-06 23:53:30 +0400

    readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adminka::Application.config.secret_token = 'eb9fdbe87e699b223b12b0f632b4d7b0ecf42a93dd7bc304a7558678a5da0ac211385ce687de0e00fafc1af00d322f81eddc34f0efe15e7fe9dee5e6ad020946'
